a266b74704b1db3db7677049810e205e351c66a4
MuhammadGheysarAbyan <gheysar2abyan@gmail.com>
2024-11-03 10:50:26 +0700

    portfolio

---
+++ js/script.js
@@ -0,0 +1,70 @@
+/*==================== TOGGLE ICON NAVBAR ====================*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active')
+} 
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+            });
+        };
+    });
+
+
+    /*==================== STICK NAVBAR ====================*/
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100)
+
+    /*==================== REMOVE TOGGLE NAVBAR ====================*/
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active')
+};
+
+
+/*==================== SCROLL REVEAL ====================*/
+ScrollReveal ({
+    // reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portofolio-box, .see-more-container, .contact', {origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img', {origin: 'left'});
+ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right'});
+
+/*==================== TYPED JS ====================*/
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'UI/UX Designer', 'Content Writer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+/*==================== CLEAR FORM ====================*/
+
+function clearForm() {
+    document.querySelector("form").reset();
+}
+
