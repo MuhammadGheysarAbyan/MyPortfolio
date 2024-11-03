1d8b951517be9e7d4a928e43bde1ee315b2f80f4
MuhammadGheysarAbyan <gheysar2abyan@gmail.com>
2024-11-03 11:32:14 +0700

    update menu icon

--- js/script.js
+++ js/script.js
@@ -1,47 +1,49 @@
 /*==================== TOGGLE ICON NAVBAR ====================*/
+
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x')
-    navbar.classList.toggle('active')
-} 
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}; 
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
+    let top = window.scrollY; 
+
     sections.forEach(sec => {
-        let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height) {
+        if (top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
             });
-        };
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        }
     });
-
 
     /*==================== STICK NAVBAR ====================*/
 
     let header = document.querySelector('header');
-
-    header.classList.toggle('sticky', window.scrollY > 100)
+    header.classList.toggle('sticky', top > 100);
 
     /*==================== REMOVE TOGGLE NAVBAR ====================*/
-    menuIcon.classList.toggle('bx-x')
-    navbar.classList.toggle('active')
+
+    // menuIcon.classList.toggle('bx-x')
+    // navbar.classList.toggle('active')
 };
 
-
 /*==================== SCROLL REVEAL ====================*/
+
 ScrollReveal ({
-    // reset: true,
+    reset: false,
     distance: '80px',
     duration: 2000,
     delay: 200
